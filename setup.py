c57d9ae68b12f3fd5ab042d33b841d1d188d6083
kuba <kuba@linux.com>
2021-10-08 21:34:33 +0100

    initial version

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(name="tal_conan_utils",
+      version="0.0.1",
+      description="Helpers to create conanfiles",
+      author="Tal Systems",
+      url="https://github.com/tal-systems/tal-conan-utils",
+      packages=["tal_conan_utils"],
+      install_requires=['conan >= 1.40.0'],
+      license="MIT"
+)
+
---
+++ tal_conan_utils.py
@@ -0,0 +1,78 @@
+from conans import ConanFile, CMake, tools
+import os
+
+
+def get_version():
+    git = tools.Git()
+    try:
+        tag = git.get_tag()
+        if tag:
+            return tag
+        return git.get_branch()
+    except:
+        return None
+
+
+def make_conan(lib_name, requirements):
+    class TalConan(ConanFile):
+        name = f"tal.{lib_name}"
+        version = get_version()
+        license = ""
+        author = ""
+        url = f"https://github.com/tal-systems/tal.{lib_name}"
+        description = ""
+        settings = "os", "compiler", "build_type", "arch"
+        options = {"shared": [True, False], "fPIC": [True, False]}
+        default_options = {"shared": False, "fPIC": True}
+        generators = "cmake"
+        requires = requirements
+
+        def config_options(self):
+            if self.settings.os == "Windows":
+                del self.options.fPIC
+
+        def _configure_cmake(self):
+            cmake = CMake(self)
+            cmake.configure(source_folder=f"tal.{lib_name}")
+            return cmake
+
+        def source(self):
+            self.run(f"git clone --depth 1 --branch {self.version} git@github.com:tal-systems/tal.{lib_name}.git")
+
+        def build(self):
+            cmake = self._configure_cmake()
+            cmake.build()
+
+        def package(self):
+            cmake = self._configure_cmake()
+            cmake.install()
+
+        def layout(self):
+            self.cpp.source.includedirs = ["lib/include"]
+            self.cpp.build.libdirs = ["build/lib"]
+            self.cpp.build.libs = [f"tal_{lib_name}"]
+            self.cpp.package.libs = [f"tal_{lib_name}"]
+    return TalConan
+
+
+def make_test_conan(lib_name):
+    class TalTestConan(ConanFile):
+        settings = "os", "compiler", "build_type", "arch"
+        generators = "cmake"
+
+        def build(self):
+            cmake = CMake(self)
+            cmake.configure()
+            cmake.build()
+
+        def imports(self):
+            self.copy("*.dll", dst="bin", src="bin")
+            self.copy("*.dylib*", dst="bin", src="lib")
+            self.copy('*.so*', dst='bin', src='lib')
+
+        def test(self):
+            if not tools.cross_building(self):
+                os.chdir("bin")
+                self.run(".%stest_app" % os.sep)
+    return TalTestConan
+
